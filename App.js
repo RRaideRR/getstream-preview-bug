053f2053d8b887f347f379cc865a6a95e98b67e9
philipp <philipp.reiter90@gmail.com>
2024-07-06 15:05:51 +0200

    init commit

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import 'react-native-gesture-handler';
+
+import { StreamChat } from 'stream-chat';
+import { useEffect, useState } from 'react';
+import {
+  Channel,
+  ChannelList,
+  Chat,
+  MessageInput,
+  MessageList,
+  OverlayProvider,
+  useMessageInputContext,
+} from 'stream-chat-expo';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+const client = StreamChat.getInstance('XXX');
 
 export default function App() {
+  const [channel, setChannel] = useState();
+  const [isClientReady, setIsClientReady] = useState(false);
+
+  useEffect(() => {
+    client.connectUser(
+      {
+        id: 'XXX',
+      },
+'XXX'    );
+
+    setIsClientReady(true);
+  }, []);
+
+  const filter = {
+    type: 'support', members: { $in: ['XXX'] }
+  };
+
+  if (!isClientReady) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1, paddingTop: 100 }}>
+      <OverlayProvider>
+        <Chat client={client} enableOfflineSupport>
+        {channel ?
+          <Channel
+            channel={channel}
+            enableOfflineSupport
+          >
+            <MessageList />
+            <MessageInput />
+          </Channel>
+          : <ChannelList filters={filter} onSelect={setChannel} />
+        }
+        </Chat>
+      </OverlayProvider>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
